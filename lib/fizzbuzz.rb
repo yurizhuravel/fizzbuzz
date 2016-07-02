d8240d16eb850232c90d6c21d04b8053fbf7df71
Yuri Zhuravel <zhuravel@gmail.com>
2016-07-02 13:07:49 +0100

    Passes tests with multiples of 3, 5, 15 and other numbers

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,10 @@
 def fizzbuzz(number)
-  number == 3? 'fizz' : 'buzz'
+  if number%15 == 0
+    'fizzbuzz'
+  elsif number%3 == 0
+    'fizz'
+  elsif number%5 == 0
+    'buzz'
+  else number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,24 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed a multiple of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(18)).to eq 'fizz'
+    expect(fizzbuzz(99)).to eq 'fizz'
   end
-  it 'returns "buzz" when passed 4' do
-    expect(fizzbuzz(4)).to eq 'buzz'
+  it 'returns "buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(10)).to eq 'buzz'
+    expect(fizzbuzz(95)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(45)).to eq 'fizzbuzz'
+    expect(fizzbuzz(75)).to eq 'fizzbuzz'
+  end
+  it 'returns passed number if it\'s not a multiple of 3 or 5' do
+    expect(fizzbuzz(7)).to eq 7
+    expect(fizzbuzz(11)).to eq 11
+    expect(fizzbuzz(88)).to eq 88
   end
 end
